8cefe147d25a6f15c36a944c1cbdeb6fc90e154b
MohamedMaherAli <mohamedmhasanein@gmail.com>
2022-05-13 09:51:53 +0200

    fixing styles

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   const location = useLocation();
   const adminUrls = location.pathname.startsWith('/admin/');
-  const maxWdith = adminUrls ? false : 'xl';
+  const maxWdith = adminUrls ? false : 'lg';
   return (
     <div
       className='App'
